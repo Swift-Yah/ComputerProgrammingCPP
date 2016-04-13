ba70241862ceb870b456b177218fede6e1021c24
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-04-13 11:09:15 -0300

    Initial Commit

---
+++ ComputerProgrammingCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ComputerProgrammingCPP
+//
+//  Created by Rafael Ferreira on 4/13/16.
+//  Copyright © 2016 Swift Yah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
